d6e28ff95f05b5222947d4358d61fe4e3395ab31
최 성필 <seongpil0948@idstrust.com>
2025-03-30 22:25:46 +0900

    init

---
+++ config/fonts.ts
@@ -0,0 +1,22 @@
+import { Fira_Code as FontMono, Inter as FontSans, Noto_Sans_KR, Noto_Serif_KR } from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+
+
+export const notoSansKr = Noto_Sans_KR({
+  subsets: ['latin'],
+  variable: '--font-noto-sans-kr',
+});
+
+export const notoSerifKr = Noto_Serif_KR({
+  subsets: ['latin'],
+  variable: '--font-noto-serif-kr',
+});
